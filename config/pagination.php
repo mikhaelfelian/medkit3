36b6ee45acd37b10fb770ec25aa5952ff5114871
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2024-12-28 12:10:59 +0700

    Updating Pagination

--- config/pagination.php
+++ config/pagination.php
@@ -1,36 +1,28 @@
 <?php
 return [
+    'template' => 'default',
+    'num_links' => 5,
     'templates' => [
         'default' => [
             'wrapper_start' => '<ul class="pagination pagination-sm m-0 float-right">',
             'wrapper_end' => '</ul>',
-            
-            'item_start' => '<li class="page-item">',
-            'item_end' => '</li>',
-            
-            'active_item_start' => '<li class="page-item active">',
-            'active_item_end' => '</li>',
-            
-            'disabled_item_start' => '<li class="page-item disabled">',
-            'disabled_item_end' => '</li>',
-            
-            'link_start' => '<a class="page-link rounded-0" href="',
-            'link_mid' => '">',
-            'link_end' => '</a>',
-            
-            'disabled_link' => '<a class="page-link rounded-0" href="#">',
-            'active_link' => '<a class="page-link rounded-0" href="#">',
-            
-            'prev_symbol' => '&laquo;',
-            'next_symbol' => '&raquo;',
-            'dots_symbol' => '...'
-        ],
-        // You can add other templates here
-        'bootstrap3' => [
-            // Bootstrap 3 specific template
-        ],
-        'tailwind' => [
-            // Tailwind specific template
+            'first_link' => '«',
+            'last_link' => '»',
+            'next_link' => '›',
+            'prev_link' => '‹',
+            'link_start' => '<li class="page-item">',
+            'link_end' => '</li>',
+            'link_tag_open' => '<a class="page-link" href="{url}">',
+            'link_tag_close' => '</a>',
+            'active_start' => '<li class="page-item active">',
+            'active_end' => '</li>',
+            'active_tag_open' => '<span class="page-link">',
+            'active_tag_close' => '</span>',
+            'disabled_start' => '<li class="page-item disabled">',
+            'disabled_end' => '</li>',
+            'disabled_tag_open' => '<span class="page-link">',
+            'disabled_tag_close' => '</span>',
+            'ellipsis' => '<li class="page-item disabled"><span class="page-link">...</span></li>'
         ]
     ]
 ]; 
