63a4d1a1f00b619c8860576ee39a615694d76924
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2024-12-31 22:37:40 +0700

    Update Radiologi Modul

--- app/controllers/RadiologiController.php
+++ app/controllers/RadiologiController.php
@@ -1,26 +1,31 @@
 <?php
-class RadiologiController extends BaseController {
+class RadiologiController extends BaseController
+{
     protected $model;
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         parent::__construct();
         $this->model = $this->loadModel('Radiologi');
         $this->loadHelper('angka');
         $this->loadHelper('debug');
     }
-    
-    public function index() {
+
+    public function index()
+    {
         try {
-            $page = (int)$this->input->get('page', 1);
+            $page = (int) $this->input->get('page', 1);
             $search = $this->input->get('search', '');
-            $perPage = (int)$this->input->get('per_page', 10);
-            
-            if ($page < 1) $page = 1;
-            if ($perPage < 1) $perPage = 10;
-            
+            $perPage = (int) $this->input->get('per_page', 10);
+
+            if ($page < 1)
+                $page = 1;
+            if ($perPage < 1)
+                $perPage = 10;
+
             $result = $this->model->searchPaginate($search, $page, $perPage);
             $deletedCount = $this->model->countDeleted();
-            
+
             return $this->view('master/radiologi/index', [
                 'title' => 'Data Radiologi',
                 'data' => $result['data'],
@@ -36,12 +41,13 @@ class RadiologiController extends BaseController {
         }
     }
 
-    public function create() {
+    public function create()
+    {
         try {
             // Load required models
             $kategoriModel = $this->loadModel('Kategori');
             $merkModel = $this->loadModel('Merk');
-            
+
             return $this->view('master/radiologi/create', [
                 'title' => 'Tambah Data Radiologi',
                 'kategoris' => $kategoriModel->getActiveRecords(),
@@ -55,57 +61,58 @@ class RadiologiController extends BaseController {
         }
     }
 
-    public function store() {
+    public function store()
+    {
         try {
             if (!$this->security->validateCSRFToken($this->input->post('csrf_token'))) {
                 throw new Exception('Invalid security token');
             }
 
             $data = [
-                'id_kategori' 	=> $this->input->post('id_kategori') ?: null,
-                'id_merk' 		=> $this->input->post('id_merk') ?: null,
-                'kode' 			=> $this->input->post('kode'),
-                'item' 			=> $this->input->post('item'),
-                'harga_beli' 	=> $this->input->post('harga_beli'),
-                'harga_jual' 	=> $this->input->post('harga_jual'),
-                'status_stok' 	=> $this->input->post('status_stok') ? '1' : '0',
-                'status_item' 	=> '4', // 4 for radiologi
-                'status' 		=> '1',
-                'created_at' 	=> date('Y-m-d H:i:s')
+                'id_kategori' => $this->input->post('id_kategori') ?: null,
+                'id_merk' => $this->input->post('id_merk') ?: null,
+                'kode' => $this->input->post('kode'),
+                'item' => $this->input->post('item'),
+                'harga_beli' => $this->input->post('harga_beli'),
+                'harga_jual' => $this->input->post('harga_jual'),
+                'status_stok' => $this->input->post('status_stok') ? '1' : '0',
+                'status_item' => '4', // 4 for radiologi
+                'status' => '1',
+                'created_at' => date('Y-m-d H:i:s')
             ];
-			
+
             // Always save remun_tipe even if empty
-            $data['remun_tipe'] = (string)$this->input->post('remun_tipe');
+            $data['remun_tipe'] = (string) $this->input->post('remun_tipe');
 
             // If remun_tipe is percentage (1), calculate nominal amount from percentage
             if ($this->input->post('remun_tipe') == '1') {
                 $hargaJual = Angka::cleanNumber($this->input->post('harga_jual'));
                 $remunPerc = Angka::cleanNumber($this->input->post('remun_perc'));
-                
+
                 $data['remun_perc'] = $this->input->post('remun_perc') ? Angka::cleanNumber($this->input->post('remun_perc')) : 0;
                 $data['remun_nom'] = round(($hargaJual * $remunPerc) / 100);
             } else if ($this->input->post('remun_tipe') == '2') {
                 $hargaJual = Angka::cleanNumber($this->input->post('harga_jual'));
                 $remunNom = Angka::cleanNumber($this->input->post('remun_nom'));
-                
+
                 $data['remun_nom'] = $this->input->post('remun_nom') ? Angka::cleanNumber($this->input->post('remun_nom')) : 0;
                 $data['remun_perc'] = $hargaJual > 0 ? min(round(($remunNom * 100) / $hargaJual), 100) : 0;
             }
 
             // Always save apres_tipe even if empty
-            $data['apres_tipe'] = (string)$this->input->post('apres_tipe');
-            
+            $data['apres_tipe'] = (string) $this->input->post('apres_tipe');
+
             // If apres_tipe is percentage (1), calculate nominal amount from percentage
             if ($this->input->post('apres_tipe') == '1') {
                 $hargaJual = Angka::cleanNumber($this->input->post('harga_jual'));
                 $apresPerc = Angka::cleanNumber($this->input->post('apres_perc'));
-                
+
                 $data['apres_perc'] = $this->input->post('apres_perc') ? Angka::cleanNumber($this->input->post('apres_perc')) : 0;
-                $data['apres_nom']  = round(($hargaJual * $apresPerc) / 100);
+                $data['apres_nom'] = round(($hargaJual * $apresPerc) / 100);
             } else if ($this->input->post('apres_tipe') == '2') {
-                $hargaJual = Angka::cleanNumber($this->input->post('harga_jual')); 
+                $hargaJual = Angka::cleanNumber($this->input->post('harga_jual'));
                 $apresNom = Angka::cleanNumber($this->input->post('apres_nom'));
-                
+
                 $data['apres_nom'] = $this->input->post('apres_nom') ? Angka::cleanNumber($this->input->post('apres_nom')) : 0;
                 $data['apres_perc'] = $hargaJual > 0 ? min(round(($apresNom * 100) / $hargaJual), 100) : 0;
             }
@@ -116,14 +123,15 @@ class RadiologiController extends BaseController {
 
             Notification::success('Data radiologi berhasil ditambahkan');
             return $this->redirect('radiologi');
-            
+
         } catch (Exception $e) {
             Notification::error($e->getMessage());
             return $this->redirect('radiologi/create');
         }
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         try {
             if (!$this->model->softDelete($id)) {
                 throw new Exception('Failed to delete data');
@@ -131,24 +139,27 @@ class RadiologiController extends BaseController {
 
             Notification::success('Data radiologi berhasil dihapus');
             return $this->redirect('radiologi');
-            
+
         } catch (Exception $e) {
             Notification::error($e->getMessage());
             return $this->redirect('radiologi');
         }
     }
 
-    public function trash() {
+    public function trash()
+    {
         try {
-            $page = (int)$this->input->get('page', 1);
+            $page = (int) $this->input->get('page', 1);
             $search = $this->input->get('search', '');
-            $perPage = (int)$this->input->get('per_page', 10);
-            
-            if ($page < 1) $page = 1;
-            if ($perPage < 1) $perPage = 10;
-            
+            $perPage = (int) $this->input->get('per_page', 10);
+
+            if ($page < 1)
+                $page = 1;
+            if ($perPage < 1)
+                $perPage = 10;
+
             $result = $this->model->getDeletedPaginate($search, $page, $perPage);
-            
+
             return $this->view('master/radiologi/trash', [
                 'title' => 'Data Radiologi Terhapus',
                 'data' => $result['data'],
@@ -163,7 +174,8 @@ class RadiologiController extends BaseController {
         }
     }
 
-    public function restore($id) {
+    public function restore($id)
+    {
         try {
             if (!$this->model->restore($id)) {
                 throw new Exception('Failed to restore data');
@@ -171,14 +183,15 @@ class RadiologiController extends BaseController {
 
             Notification::success('Data radiologi berhasil dipulihkan');
             return $this->redirect('radiologi/trash');
-            
+
         } catch (Exception $e) {
             Notification::error($e->getMessage());
             return $this->redirect('radiologi/trash');
         }
     }
 
-    public function permanentDelete($id) {
+    public function permanentDelete($id)
+    {
         try {
             if (!$this->model->permanentDelete($id)) {
                 throw new Exception('Failed to delete data permanently');
@@ -186,18 +199,19 @@ class RadiologiController extends BaseController {
 
             Notification::success('Data radiologi berhasil dihapus permanen');
             return $this->redirect('radiologi/trash');
-            
+
         } catch (Exception $e) {
             Notification::error($e->getMessage());
             return $this->redirect('radiologi/trash');
         }
     }
 
-    public function edit($id) {
+    public function edit($id)
+    {
         try {
             // Get radiologi data
             $data = $this->model->find($id);
-			
+
             if (!$data) {
                 throw new Exception('Data radiologi tidak ditemukan');
             }
@@ -218,44 +232,45 @@ class RadiologiController extends BaseController {
         }
     }
 
-    public function update($id) {
+    public function update($id)
+    {
         try {
             // Validate input
             $data = [
-                'id_kategori' 	=> $this->input->post('id_kategori'),
-                'item' 			=> $this->input->post('item'),
-                'harga_jual' 	=> $this->input->post('harga_jual'),
-                'remun_tipe' 	=> $this->input->post('remun_tipe') ?: '0',
-                'apres_tipe' 	=> $this->input->post('apres_tipe') ?: '0',
-                'status_stok' 	=> $this->input->post('status_stok') ? '1' : '0'
-            ];	
+                'id_kategori' => $this->input->post('id_kategori'),
+                'item' => $this->input->post('item'),
+                'harga_jual' => $this->input->post('harga_jual'),
+                'remun_tipe' => $this->input->post('remun_tipe') ?: '0',
+                'apres_tipe' => $this->input->post('apres_tipe') ?: '0',
+                'status_stok' => $this->input->post('status_stok') ? '1' : '0'
+            ];
 
             // If remun_tipe is percentage (1), calculate nominal amount from percentage
             if ($this->input->post('remun_tipe') == '1') {
                 $hargaJual = Angka::cleanNumber($this->input->post('harga_jual'));
                 $remunPerc = Angka::cleanNumber($this->input->post('remun_perc'));
-                
+
                 $data['remun_perc'] = $this->input->post('remun_perc') ? Angka::cleanNumber($this->input->post('remun_perc')) : 0;
                 $data['remun_nom'] = round(($hargaJual * $remunPerc) / 100);
             } else if ($this->input->post('remun_tipe') == '2') {
                 $hargaJual = Angka::cleanNumber($this->input->post('harga_jual'));
                 $remunNom = Angka::cleanNumber($this->input->post('remun_nom'));
-                
+
                 $data['remun_nom'] = $this->input->post('remun_nom') ? Angka::cleanNumber($this->input->post('remun_nom')) : 0;
                 $data['remun_perc'] = $hargaJual > 0 ? min(round(($remunNom * 100) / $hargaJual), 100) : 0;
             }
-            
+
             // If apres_tipe is percentage (1), calculate nominal amount from percentage
             if ($this->input->post('apres_tipe') == '1') {
                 $hargaJual = Angka::cleanNumber($this->input->post('harga_jual'));
                 $apresPerc = Angka::cleanNumber($this->input->post('apres_perc'));
-                
+
                 $data['apres_perc'] = $this->input->post('apres_perc') ? Angka::cleanNumber($this->input->post('apres_perc')) : 0;
-                $data['apres_nom']  = round(($hargaJual * $apresPerc) / 100);
+                $data['apres_nom'] = round(($hargaJual * $apresPerc) / 100);
             } else if ($this->input->post('apres_tipe') == '2') {
-                $hargaJual = Angka::cleanNumber($this->input->post('harga_jual')); 
+                $hargaJual = Angka::cleanNumber($this->input->post('harga_jual'));
                 $apresNom = Angka::cleanNumber($this->input->post('apres_nom'));
-                
+
                 $data['apres_nom'] = $this->input->post('apres_nom') ? Angka::cleanNumber($this->input->post('apres_nom')) : 0;
                 $data['apres_perc'] = $hargaJual > 0 ? min(round(($apresNom * 100) / $hargaJual), 100) : 0;
             }
@@ -278,4 +293,4 @@ class RadiologiController extends BaseController {
             return $this->redirect('radiologi/edit/' . $id);
         }
     }
-} 
+}
--- app/models/RadiologiModel.php
+++ app/models/RadiologiModel.php
@@ -1,8 +1,9 @@
 <?php
-class RadiologiModel extends BaseModel {
+class RadiologiModel extends BaseModel
+{
     protected $table = 'tbl_m_items';
     protected $primaryKey = 'id';
-    
+
     protected $fillable = [
         'kode',
         'id_kategori',
@@ -26,33 +27,34 @@ class RadiologiModel extends BaseModel {
         'updated_at'
     ];
 
-    public function searchPaginate($search = '', $page = 1, $perPage = 10) {
+    public function searchPaginate($search = '', $page = 1, $perPage = 10)
+    {
         try {
             $offset = ($page - 1) * $perPage;
             $conditions = ["i.status_hps = '0' AND i.status_item = '4'"]; // Only show radiologi items
             $params = [];
-            
+
             if (!empty($search)) {
                 $conditions[] = "(i.kode LIKE :search OR i.item LIKE :search OR k.kategori LIKE :search OR m.merk LIKE :search)";
                 $params[':search'] = "%{$search}%";
             }
-            
+
             $where = 'WHERE ' . implode(' AND ', $conditions);
-            
+
             // Get total records
             $countSql = "SELECT COUNT(*) as total 
                          FROM {$this->table} i 
                          LEFT JOIN tbl_m_kategoris k ON i.id_kategori = k.id 
                          LEFT JOIN tbl_m_merks m ON i.id_merk = m.id 
                          {$where}";
-            
+
             $stmt = $this->conn->prepare($countSql);
             foreach ($params as $key => $value) {
                 $stmt->bindValue($key, $value);
             }
             $stmt->execute();
             $total = $stmt->fetch(PDO::FETCH_OBJ)->total;
-            
+
             // Get paginated records
             $sql = "SELECT i.*, 
                            k.kategori as nama_kategori,
@@ -63,31 +65,32 @@ class RadiologiModel extends BaseModel {
                     {$where} 
                     ORDER BY i.created_at DESC 
                     LIMIT :limit OFFSET :offset";
-            
+
             $stmt = $this->conn->prepare($sql);
             $stmt->bindValue(':limit', $perPage, PDO::PARAM_INT);
             $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
-            
+
             foreach ($params as $key => $value) {
                 $stmt->bindValue($key, $value);
             }
-            
+
             $stmt->execute();
-            
+
             return [
                 'data' => $stmt->fetchAll(PDO::FETCH_OBJ),
                 'total' => $total,
                 'per_page' => $perPage,
                 'current_page' => $page
             ];
-            
+
         } catch (PDOException $e) {
             error_log("Database Error in searchPaginate: " . $e->getMessage());
             throw new Exception("Failed to fetch radiologi data");
         }
     }
 
-    public function countDeleted() {
+    public function countDeleted()
+    {
         try {
             $sql = "SELECT COUNT(*) as total FROM {$this->table} 
                     WHERE status_hps = '1' AND status_item = '4'";
@@ -99,23 +102,24 @@ class RadiologiModel extends BaseModel {
         }
     }
 
-    public function generateKode() {
+    public function generateKode()
+    {
         try {
             $year = date('y');
             $month = date('m');
-            
+
             // Get last number from this month for radiologi (RD prefix)
             $sql = "SELECT kode FROM {$this->table} 
                     WHERE kode LIKE :prefix 
                     AND status_item = '4' 
                     ORDER BY kode DESC LIMIT 1";
-                    
+
             $stmt = $this->conn->prepare($sql);
             $stmt->bindValue(':prefix', "RD{$year}{$month}%");
             $stmt->execute();
-            
+
             $data = $stmt->fetch(PDO::FETCH_OBJ);
-            
+
             if ($data) {
                 // Extract the number part and increment
                 $lastNumber = intval(substr($data->kode, -4));
@@ -124,113 +128,118 @@ class RadiologiModel extends BaseModel {
                 // If no existing code, start with 1
                 $newNumber = 1;
             }
-            
+
             // Generate new code with padding
             return "RD{$year}{$month}" . str_pad($newNumber, 4, '0', STR_PAD_LEFT);
-            
+
         } catch (PDOException $e) {
             error_log("Database Error in generateKode: " . $e->getMessage());
             throw new Exception("Failed to generate kode");
         }
     }
 
-    public function softDelete($id) {
+    public function softDelete($id)
+    {
         try {
             $sql = "UPDATE {$this->table} 
                     SET status_hps = '1', 
                         deleted_at = :deleted_at 
                     WHERE id = :id";
-                    
+
             $stmt = $this->conn->prepare($sql);
             $stmt->bindValue(':id', $id);
             $stmt->bindValue(':deleted_at', date('Y-m-d H:i:s'));
-            
+
             return $stmt->execute();
-            
+
         } catch (PDOException $e) {
             error_log("Database Error in softDelete: " . $e->getMessage());
             throw new Exception("Failed to delete record");
         }
     }
 
-    public function getDeletedPaginate($search = '', $page = 1, $perPage = 10) {
+    public function getDeletedPaginate($search = '', $page = 1, $perPage = 10)
+    {
         try {
             $offset = ($page - 1) * $perPage;
             $conditions = ["status_hps = '1'"];
-            
+
             if (!empty($search)) {
                 $conditions[] = "(kode LIKE :search OR item LIKE :search)";
             }
-            
+
             $where = implode(' AND ', $conditions);
-            
+
             // Get total records
             $sql = "SELECT COUNT(*) as total FROM {$this->table} WHERE {$where}";
             $stmt = $this->conn->prepare($sql);
-            
+
             if (!empty($search)) {
                 $stmt->bindValue(':search', "%{$search}%");
             }
-            
+
             $stmt->execute();
             $total = $stmt->fetch(PDO::FETCH_OBJ)->total;
-            
+
             // Get paginated records
             $sql = "SELECT * FROM {$this->table} WHERE {$where} ORDER BY created_at DESC LIMIT :offset, :limit";
             $stmt = $this->conn->prepare($sql);
-            
+
             if (!empty($search)) {
                 $stmt->bindValue(':search', "%{$search}%");
             }
-            
+
             $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
             $stmt->bindValue(':limit', $perPage, PDO::PARAM_INT);
             $stmt->execute();
-            
+
             return [
                 'data' => $stmt->fetchAll(PDO::FETCH_OBJ),
                 'total' => $total
             ];
-            
+
         } catch (PDOException $e) {
             error_log("Database Error in getDeletedPaginate: " . $e->getMessage());
             throw new Exception("Failed to fetch deleted records");
         }
     }
 
-    public function restore($id) {
+    public function restore($id)
+    {
         try {
             $sql = "UPDATE {$this->table} 
                     SET status_hps = '0', 
                         deleted_at = NULL 
                     WHERE id = :id";
-                    
+
             $stmt = $this->conn->prepare($sql);
             $stmt->bindValue(':id', $id);
-            
+
             return $stmt->execute();
-            
+
         } catch (PDOException $e) {
             error_log("Database Error in restore: " . $e->getMessage());
             throw new Exception("Failed to restore record");
         }
     }
 
-    public function permanentDelete($id) {
+    public function permanentDelete($id)
+    {
         try {
             $sql = "DELETE FROM {$this->table} WHERE id = :id AND status_hps = '1'";
             $stmt = $this->conn->prepare($sql);
             $stmt->bindValue(':id', $id);
-            
+
             return $stmt->execute();
-            
+
         } catch (PDOException $e) {
             error_log("Database Error in permanentDelete: " . $e->getMessage());
             throw new Exception("Failed to permanently delete record");
         }
     }
 
-    public function validate($data, $id = null) {
+    public function validate($data, $id = null)
+    {
         $errors = [];
 
         // Validate id_kategori
@@ -247,7 +256,7 @@ class RadiologiModel extends BaseModel {
             if ($id) {
                 $sql .= " AND id != :id";
             }
-            
+
             try {
                 $stmt = $this->conn->prepare($sql);
                 $stmt->bindValue(':item', $data['item']);
@@ -256,7 +265,7 @@ class RadiologiModel extends BaseModel {
                 }
                 $stmt->execute();
                 $result = $stmt->fetch(PDO::FETCH_OBJ);
-                
+
                 if ($result->count > 0) {
                     $errors['item'] = 'Nama tindakan sudah digunakan';
                 }
@@ -305,4 +314,4 @@ class RadiologiModel extends BaseModel {
 
         return $errors;
     }
-} 
+}
